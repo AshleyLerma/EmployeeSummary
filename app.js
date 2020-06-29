30c1360ddb8c8d41a69713e1dfa1397b4ae3f605
Ashley Lerma <lerma.scout@gmail.com>
2020-06-29 13:00:40 -0500

    basic employee questions asked

--- app.js
+++ app.js
@@ -12,10 +12,44 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function promptUser() {
+  return inquirer.prompt([
+    {
+      name: "role",
+      type: "checkbox",
+      choices: ["Manager", "Engineer", "Intern"],
+      message: "Please select the employee's role.",
+    },
+    {
+      name: "name",
+      type: "input",
+      message: "Employee name:",
+      default: "Employee Name",
+    },
+    {
+      name: "id",
+      type: "input",
+      message: "Employee ID:",
+      default: "Employee ID",
+    },
+  ]);
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+promptUser()
+  .then(function (answers) {
+    const html = render(answers);
+    return writeFileAsync("team.html", html);
+  })
+  .then(function () {
+    console.log("Successfully wrote to team.html");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
