c81f440a728915a0355a4ed636f3333f73e64a30
Ashley Lerma <lerma.scout@gmail.com>
2020-06-30 12:16:44 -0500

    added comments and call render() passing in employees

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// All variables and required connections to other pages
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,92 +11,103 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-let manager;
+// Blank employees array to hold all employee objects
 let employees = [];
 
+// Function to add employees
 function promptUser() {
-  return inquirer
-    .prompt([
-      {
-        name: "name",
-        type: "input",
-        message: "Employee name:",
-        default: "Employee Name",
-      },
-      {
-        name: "role",
-        type: "list",
-        choices: ["Manager", "Engineer", "Intern"],
-        message: "Please select the employee's role.",
-      },
-      {
-        name: "id",
-        type: "input",
-        message: "Employee ID:",
-        default: "Employee ID",
-      },
-      {
-        name: "email",
-        type: "input",
-        message: "Employee email:",
-        default: "Employee Email",
-      },
-      {
-        name: "office",
-        type: "input",
-        message: "What is the Manager's office number?",
-        when: (userInput) => userInput.role === "Manager",
-      },
-      {
-        name: "github",
-        type: "input",
-        message: "What is the Engineer's GitHub username?",
-        when: (userInput) => userInput.role === "Engineer",
-      },
-      {
-        name: "school",
-        type: "input",
-        message: "What's the Intern's school?",
-        when: (userInput) => userInput.role === "Intern",
-      },
-      {
-        name: "newEmployee",
-        type: "confirm",
-        message: "Would you like to add another employee?",
-      },
-    ])
-    .then((answers) => {
-      switch (answers.role) {
-        case "Manager": {
-          manager = new Manager(
-            answers.name,
-            answers.id,
-            answers.email,
-            answers.office
-          );
+  return (
+    inquirer
+      .prompt([
+        {
+          name: "name",
+          type: "input",
+          message: "Employee name:",
+          default: "Employee Name",
+        },
+        {
+          name: "role",
+          type: "list",
+          choices: ["Manager", "Engineer", "Intern"],
+          message: "Please select the employee's role.",
+        },
+        {
+          name: "id",
+          type: "input",
+          message: "Employee ID:",
+          default: "Employee ID",
+        },
+        {
+          name: "email",
+          type: "input",
+          message: "Employee email:",
+          default: "Employee Email",
+        },
+        {
+          name: "office",
+          type: "input",
+          message: "What is the Manager's office number?",
+          when: (userInput) => userInput.role === "Manager",
+        },
+        {
+          name: "github",
+          type: "input",
+          message: "What is the Engineer's GitHub username?",
+          when: (userInput) => userInput.role === "Engineer",
+        },
+        {
+          name: "school",
+          type: "input",
+          message: "What's the Intern's school?",
+          when: (userInput) => userInput.role === "Intern",
+        },
+        {
+          name: "newEmployee",
+          type: "confirm",
+          message: "Would you like to add another employee?",
+        },
+      ])
+      // Use answers to create an employee that is pushed to the employees array
+      .then((answers) => {
+        switch (answers.role) {
+          case "Manager": {
+            employees.push(
+              new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.office
+              )
+            );
+          }
+          case "Engineer": {
+            employees.push(
+              new Engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.github
+              )
+            );
+          }
+          case "Intern":
+            employees.push(
+              new Intern(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.school
+              )
+            );
         }
-        case "Engineer": {
-          employees.push(
-            new Engineer(
-              answers.name,
-              answers.id,
-              answers.email,
-              answers.github
-            )
-          );
+        // If they say yes to adding more employees reprompt, otherwise render html passing in the employees array
+        if (answers.newEmployee === true) {
+          promptUser();
+        } else {
+          render(employees);
         }
-        case "Intern":
-          employees.push(
-            new Intern(answers.name, answers.id, answers.email, answers.school)
-          );
-      }
-      if (answers.newEmployee === true) {
-        promptUser();
-      } else {
-        // render html
-        console.log("time to render");
-      }
-    });
+      })
+  );
 }
 
 promptUser();
@@ -109,9 +121,3 @@ promptUser();
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
